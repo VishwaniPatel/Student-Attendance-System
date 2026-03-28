4af4c9249b0f173aa3f79124df889ac547da3044
Vishwani Patel <vishwanipatel494@gmail.com>
2026-03-28 14:19:24 +0530

    feat: created folder structure for frontend

---
+++ client/src/pages/students/details/AttendanceSummary.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AttendanceSummary = () => {
+  return (
+    <div>AttendanceSummary</div>
+  )
+}
+
+export default AttendanceSummary
---
+++ client/src/pages/students/list/components/AttendanceToggle.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AttendanceToggle = () => {
+  return (
+    <div>AttendanceToggle</div>
+  )
+}
+
+export default AttendanceToggle
